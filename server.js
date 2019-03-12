2de07b2eee62a747d3b1c8b1977b35ca56de8268
Caitlin McCarron-Haber <Caitlin@Caitlins-MacBook-Pro.local>
2019-03-11 22:41:32 -0400

    pushThree

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 var express = require("express");
+
+var app = express();
+
 var PORT = process.env.PORT || 8080;
-var exphbs + require("express-handlebars");
+
+var db = require("./models");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 var app = express();
 
