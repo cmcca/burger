c112f038ed2d03992cdd00c5eb13fae991ab704d
Caitlin McCarron-Haber <Caitlin@Caitlins-MacBook-Pro.local>
2019-03-11 22:31:51 -0400

    pustTwo

--- config/connection.js
+++ config/connection.js
@@ -3,3 +3,22 @@
 
 * Export the connection.
 */
+
+var mysq = require("mysql");
+
+var connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "roog",
+    password: "",
+    database: "burgers_db"
+});
+
+connection.connect(function(err) {
+    if(err) {
+        return;
+    }
+    console.log("connected as id: " + connection.threadId);
+});
+
+module.exports = connection;
